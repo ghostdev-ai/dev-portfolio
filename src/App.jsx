3e025ea22e7036b26d40123cc845e380091d6945
Ulises Gomez <usg@arizona.edu>
2024-01-15 19:57:40 -0800

    cleaned up code and styling, deleted files to restart website design

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import { Box, Stack } from '@chakra-ui/react';
-import { Navbar } from './components/Navbar'
 import { Intro } from './components/Intro'
-import { Portfolio } from './components/Portfolio'
-import { Timeline } from './components/Timeline'
-import { Contact } from './components/Contact'
+import { Portfolio} from './components/Portfolio'
 import { Footer } from './components/Footer'
 import './App.css'
 
 function App() {
   return (
     <Stack spacing={0}>
-      <Navbar />
       <Intro />
       <Portfolio />
-      <Timeline />
-      <Contact />
-      <Footer />
     </Stack>
   )
 }
